614b67afb8a12576f3c9ec1af6704b896f5b9422
Fahad Hassan <fahadhassan0001@gmail.com>
2024-09-14 03:18:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var number = Number(prompt("Enter the number"));
+if(number%2 === 0) {
+    alert("The Number " + number +  " is Even \n" + "After incrementing, the number is " + ++number)
+}
+else{
+    alert("The Number " + number +  " is Odd \n" + "After incrementing, the number is " + ++number)
+}
